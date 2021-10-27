2771839b66ea1070a60f645cc336dedd6cfb480b
Mario Cerretti <mario.cerretti@gmail.com>
2021-10-27 20:06:23 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { HashRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import { ChartComp } from "./components/ChartComp";
+import { About } from "./components/About";
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello World!</h1>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={ChartComp}></Route>
+                    <Route path="/about" component={About}></Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
